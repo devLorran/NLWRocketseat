c7231b1403f17f8fe883394e5255fb1c38dece91
Lorran Rodrigues <62362231+devLorran@users.noreply.github.com>
2021-02-28 16:59:29 -0300

    Update AnswerController.ts

--- aulas/api/src/controllers/AnswerController.ts
+++ aulas/api/src/controllers/AnswerController.ts
@@ -1,29 +1,51 @@
+import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
-import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
-import { Request, Response } from 'express';
 import { AppError } from "../errors/AppErrors";
+import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
+import * as yup from 'yup'
 
-class AnswerController{
-    async execute(request: Request, response: Response){
-        const { value } = request.params;
-        const { u } = request.query;
+class AnswerController {
+  async execute(req: Request, res: Response) {
 
-        const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+    const paramSchema = yup.object().shape({
+      value: yup
+        .number()
+        .min(1, "Informe uma resposta entre 0 e 10")
+        .max(10, "Informe uma resposta entre 0 e 10")
+        .required('É necessário informar a resposta')
+    })
+    const querySchema = yup.object().shape({
+      survey: yup
+        .string()
+        .uuid("A identificação da questão deverá ser um UUID válido")
+        .required('É necessário informar a identificação da pesquisa')
+    })
 
-        const surveyUser = await SurveysUsersRepository.findOne({
-            id:String(u)
-        });
+    await Promise.all([
+      paramSchema.validate(req.params, {abortEarly: false}),
+      querySchema.validate(req.query, {abortEarly: false})
+    ])
 
-        if(!surveyUser){
-            throw new AppError("Survey User does not exists", 400);
-        }
+    const { value } = req.params;
+    const { survey } = req.query;
+    
+    const surveysUsersRepository = getCustomRepository(SurveysUsersRepository)
 
-        surveyUser.value = Number(value);
+    const surveyUser = await SurveysUsersRepository.findOne({
+      id: String(survey)
+    })
 
-        await surveysUsersRepository.save(surveyUser);
-
-        return response.json(surveyUser);
+    if (!surveyUser) {
+      throw new AppError("Survey User does not exists!");
     }
+    
+    surveyUser.value = Number(value);
+    console.log('Registering answer', surveyUser)
 
+    await SurveysUsersRepository.save(surveyUser)
+
+    return res.status(200).json(surveyUser)
+  }
 }
+
 export { AnswerController }
